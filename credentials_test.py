bb510ad4dbf093ffbd490afe59753b1f8200ce62
ombaejr <ombaejr@gmail.com>
2019-06-17 05:11:36 +0300

    test to check for proper creation of a new user

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,23 @@
 import unittest
 import pyperclip
-from credentials import User, Credential
+from user_credentials import User, Credential
+
+class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def setUp(self):
+		'''
+		Function to create a user account before each test
+		'''
+		self.new_user = User('Seth','Ombae,'pswd100')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name,'Seth')
+		self.assertEqual(self.new_user.last_name,'Ombae')
+		self.assertEqual(self.new_user.password,'pswd100')
