449539761e19e2a54af95dc8e2217afb3f8abd3a
ombaejr <ombaejr@gmail.com>
2019-06-17 04:53:44 +0300

    Include function for saving user credentials and generating a password

--- credentials.py
+++ credentials.py
@@ -1,4 +1,3 @@
-
 import pyperclip
 import random
 import string
@@ -45,3 +44,28 @@ class Credential:
 			if (user.first_name == first_name and user.password == password):
 				current_user = user.first_name
 		return current_user
+
+	def __init__(self,user_name,site_name,account_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
+
+	def save_credentials(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		# global users_list
+		Credential.credentials_list.append(self)
+
+	def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate an 8 character password for a credential
+		'''
+		gen_pass=''.join(random.choice(char) for _ in range(size))
+		return gen_pass
